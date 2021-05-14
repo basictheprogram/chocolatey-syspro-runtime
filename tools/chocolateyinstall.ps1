1b205bdf1f5525fffa8dbcc2f45d4ded4de6be0f
Bob Tanner <tanner@real-time.com>
2021-05-14 15:50:24 -0500

    Closes: #5
      * Change to url and url64
      * Clean and organization

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,33 +1,25 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation   = Join-Path $toolsDir 'SYSPRO Runtime 32-bit.msi'
-$fileLocation64 = Join-Path $toolsDir 'SYSPRO Runtime 64-bit.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI' 
-  file          = $fileLocation
-  file64        = $fileLocation64
-  softwareName  = 'syspro-runtime*'
+  fileType      = 'exe'
+  silentArgs    = '/S /v/qn'
+  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{704F9975-E977-4AEB-BF57-6E5D6EDB6781}/SYSPRO Runtime 32-bit.exe'
+  url64         = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{68AC2C62-2E32-49D5-BE62-FFBDFB8B002E}/SYSPRO Runtime 64-bit.exe'
+  softwareName  = 'syspro-runtime'
 
-  # Checksums are now required as of 0.10.0.
-  #
   # SYSPRO Runtime 32-bit.msi
-  checksum      = '22b025522196abc3097b76d6d5e4c803c77cc7f9612d3b6c3e3923e11f606933'
+  checksum      = '136fe58cb8f11b8d0702502e978f0f8a8986d2614ceef5a8d79649c20d9c745e'
   checksumType  = 'sha256'
-  # SYSPRO Runtime 64-bit.msi
-  checksum64    = 'c62d19c9daa17378803c3dd1f7938ab5acc821378dcf574ef060ae78da0846b0'
-  checksumType64= 'sha256'
 
-  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
-  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
-  #
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
-  validExitCodes= @(0, 3010, 1641)
+  # SYSPRO Runtime 64-bit.msi
+  checksum64    = '63cb9cec570d4c8242189f17c4a03546eef4fa3e4c1da2c6dc4626dd59522787'
+  checksumType64= 'sha256'
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
 #
-Install-ChocolateyInstallPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
