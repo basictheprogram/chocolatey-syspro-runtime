36f185b63c934d118b39e1fe5a2d8b4a5d9a17a4
Bob Tanner <tanner@real-time.com>
2021-06-10 16:25:26 -0500

    WIP: https://gitlab.x.real-time.com/chocolatey/syspro-client/issues/8
      * Moved private data into data.ps1
      * Ignore data.ps1 so it does not get committed to git

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,23 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  silentArgs    = '/S /v/qn'
-  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{704F9975-E977-4AEB-BF57-6E5D6EDB6781}/SYSPRO Runtime 32-bit.exe'
-  url64         = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{68AC2C62-2E32-49D5-BE62-FFBDFB8B002E}/SYSPRO Runtime 64-bit.exe'
-  softwareName  = 'syspro-runtime'
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    silentArgs     = '/S /v/qn'
+    url            = $data.url
+    url64          = $data.url64
+    softwareName   = 'syspro-runtime'
 
-  # SYSPRO Runtime 32-bit.msi
-  checksum      = '136fe58cb8f11b8d0702502e978f0f8a8986d2614ceef5a8d79649c20d9c745e'
-  checksumType  = 'sha256'
+    # SYSPRO Runtime 32-bit.msi
+    checksum       = $data.checksum
+    checksumType   = $data.checksumType
 
-  # SYSPRO Runtime 64-bit.msi
-  checksum64    = '63cb9cec570d4c8242189f17c4a03546eef4fa3e4c1da2c6dc4626dd59522787'
-  checksumType64= 'sha256'
+    # SYSPRO Runtime 64-bit.msi
+    checksum64     = $data.checksum64
+    checksumType64 = $data.checksumType64
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
